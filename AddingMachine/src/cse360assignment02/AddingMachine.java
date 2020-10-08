5d57a65bc79463d03cc6b1e33aed4337c6a90cf5
Brandon Evans <bevans1999@gmail.com>
2020-10-07 19:19:27 -0700

    AddingMachine commit 1
    First commit for Assignment 2

---
+++ AddingMachine/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,54 @@
+package cse360assignment02;
+
+/**
+ * 
+ * @author Brandon Evans
+ * This is a program for <b>Assignment 2</b> for CSE360
+ * The purpose is to help get students familiar with version control
+ */
+public class AddingMachine {
+	private int total;
+	
+	/**
+	 * The AddingMachine constructor
+	 * 
+	 */
+	public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Method to retrieve total
+	 * @return
+	 */
+	public int getTotal () {
+	    return 0;
+	}
+	
+	/**
+	 * Method to add values
+	 * @param value - an integer value for addition
+	 */
+	public void add (int value) {
+	}
+	
+	/**
+	 * Method to subtract values
+	 * @param value - an integer value for subtraction
+	 */
+	public void subtract (int value) {
+	}
+	
+	/**
+	 * Method to create string with key data from class
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Method to clear the class
+	 */
+	public void clear() {
+	}
+}
